2a25cd4d2b3c46eab5d9a09a57402c4469141fa7
김태윤 <xodbs9810@gmail.com>
2025-10-29 14:50:56 +0900

    feat : Lotto클래스 기능추가
    validate 2개 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,12 +1,16 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validate2(numbers);
+        validate3(numbers);
         this.numbers = numbers;
     }
 
@@ -14,6 +18,21 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+    }
+    //중복 있으면 튕구기
+    private void validate2(List<Integer> numbers) {
+        Set<Integer> set = new HashSet<>(numbers);
+        if (set.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되면 안 됩니다.");
+        }
+
+    }
+    //값 벗어나면 튕구기
+    private void validate3(List<Integer> numbers) {
+        if(numbers.stream().anyMatch(number -> number < 1 || number > 45)){
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1과 45사이에 있어야합니다.");
+        }
+
     }
 
     // TODO: 추가 기능 구현
